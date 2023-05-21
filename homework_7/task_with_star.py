8e8fa3c6917f57326d9998b8b4f22384603365f8
Zhanna Arobelidze <arobelidzezhuzhuna@gmail.com>
2023-05-21 16:34:56 +0300

    Домашка 7

---
+++ homework_7/task_with_star.py
@@ -0,0 +1,73 @@
+# Напишите класс RomanNums
+# Экземпляр класса создается из строки - Римского числа.
+# Реализуйте методы класса:
+# 1. from_roman, который переводит римскую запись числа в арабскую
+# 2. is_palindrome, метод определяет, является ли арабское число палиндромом (True - является, иначе False)
+# т.е. имеет ли одинаковое значение число при чтении слева направо и справа налево
+# Например (Ввод --> Вывод) :
+# RomanNums('MMMCCLXIII').from_roman() --> 3263
+# RomanNums('CMXCIX').is_palindrome() --> True
+
+# Здесь пишем код
+class RomanNums:
+
+    ROMAN_SYM = {"I": 1, "IV": 4, "V": 5, "IX": 9, "X": 10, "XL": 40, "L": 50, "XC": 90, "C": 100, "CD": 400,
+                 "D": 500, "CM": 900, "M": 1000}
+
+    def __init__(self, roman_str):
+        self.roman_str = roman_str
+
+    def from_roman(self):
+        arabic_number, i = 0, 0
+        while i < len(self.roman_str):
+            if self.roman_str[i:i+2] in self.ROMAN_SYM.keys():
+                arabic_number += self.ROMAN_SYM[self.roman_str[i:i+2]]
+                i += 2
+            else:
+                arabic_number += self.ROMAN_SYM[self.roman_str[i]]
+                i += 1
+        return arabic_number
+
+    def is_palindrome(self):
+        arabic_number = self.from_roman()
+        revers_number = list(str(arabic_number))
+        revers_number.reverse()
+        if arabic_number == int(''.join(revers_number)):
+            return True
+        return False
+# Ниже НИЧЕГО НЕ НАДО ИЗМЕНЯТЬ
+
+
+data = [RomanNums('MMMCCLXIII').from_roman,
+        RomanNums('CXXXIV').from_roman,
+        RomanNums('LXXXVI').from_roman,
+        RomanNums('MCDV').from_roman,
+        RomanNums('CMLXXVIII').from_roman,
+        RomanNums('MMMCDIV').from_roman,
+        RomanNums('CMX').from_roman,
+        RomanNums('MMCCCLXXXVIII').from_roman,
+        RomanNums('MMVIII').from_roman,
+        RomanNums('MCLXXIX').from_roman,
+        RomanNums('MMMDCCXCV').from_roman,
+        RomanNums('CMLXXXVIII').from_roman,
+        RomanNums('CMXCIX').from_roman,
+        RomanNums('CDXLIV').from_roman,
+        RomanNums('CMXCIX').is_palindrome,
+        RomanNums('CDXLIV').is_palindrome,
+        RomanNums('MMMCCLXIII').is_palindrome,
+        RomanNums('CXXXIV').is_palindrome,
+        RomanNums('V').is_palindrome,
+        RomanNums('MI').is_palindrome,
+        RomanNums('XXX').is_palindrome,
+        RomanNums('D').is_palindrome,
+        ]
+
+
+test_data = [3263, 134, 86, 1405, 978, 3404, 910, 2388, 2008, 1179, 3795, 988, 999, 444,
+             True, True, False, False, True, True, False, False]
+
+for i, d in enumerate(data):
+    assert_error = f'Не прошла проверка для метода {d.__qualname__} экземпляра с атрибутами {d.__self__.__dict__}'
+    assert d() == test_data[i], assert_error
+    print(f'Набор для метода {d.__qualname__} экземпляра класса с атрибутами {d.__self__.__dict__} прошёл проверку')
+print('Всё ок')
